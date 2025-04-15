0569aae1f41155274d57a4c15f2e1ae6143ae22d
lys <1402175410@qq.com>
2025-04-15 21:34:20 +0800

    fix

--- components/MainSwiper.tsx
+++ components/MainSwiper.tsx
@@ -21,39 +21,41 @@ export default function MainSwiper({
   sliderData: Array<{ src: string; href: string }>;
 }) {
   return (
-    <Swiper
-      className="my-custom-swiper w-full"
-      loop
-      spaceBetween={5}
-      slidesPerView={1}
-      autoplay={{
-        delay: 5000,
-        disableOnInteraction: true,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Pagination, Navigation, Autoplay]}
-    >
-      {sliderData.map((item, idx) => {
-        const src = `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_PUBLIC_ACCESS_URL}${item.src}`;
-        return (
-          <SwiperSlide key={idx}>
-            <Link className="w-full" href={item.href}>
-              <Image
-                alt={item.src}
-                src={src}
-                width={1920}
-                height={1080}
-                priority={true}
-                className="object-scale-down"
-                unoptimized
-              />
-            </Link>
-          </SwiperSlide>
-        );
-      })}
-    </Swiper>
+    <div className="bg-gray-100 dark:bg-transparent">
+      <Swiper
+        className="my-custom-swiper w-full max-w-[1200px] max-h-[966px]"
+        loop
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: true,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation, Autoplay]}
+      >
+        {sliderData.map((item, idx) => {
+          const src = `${process.env.NEXT_PUBLIC_CLOUDFLARE_R2_PUBLIC_ACCESS_URL}${item.src}`;
+          return (
+            <SwiperSlide key={idx}>
+              <Link className="w-full" href={item.href}>
+                <Image
+                  alt={item.src}
+                  src={src}
+                  width={1920}
+                  height={1080}
+                  priority={true}
+                  className="object-scale-down"
+                  unoptimized
+                />
+              </Link>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
   );
 }
